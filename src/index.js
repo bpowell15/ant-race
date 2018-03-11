7e93789c67baf8a43dc760708e1617715e7a7a75
bpowell15 <bpaulpowell@gmail.com>
2018-03-11 11:43:13 -0400

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AntRace from './components/antRace';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<AntRace />, document.getElementById('root'));
-registerServiceWorker();
